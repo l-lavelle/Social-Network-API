10fba62753421b74120cbd158508a62ee7c2389b
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-05 20:03:48 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-//Express.js for routing, a MongoDB database, and the Mongoose ODM
-// optionally use a JavaScript date library of your choice or the native JavaScript Date object to format timestamps
+// Require dependencies
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
@@ -11,13 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// Connect to MongoDB database and then listen on specified port
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
-// const moment = require("moment");
-// function formatDate(createdAt) {
-//   return moment(createdAt).format(`MM-DD-YYYY`);
-// }
-// console.log(formatDate("2023-10-03T22:30:48.966Z"));
